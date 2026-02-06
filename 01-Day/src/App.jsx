14e5fbe228d82a36d32d32e8455f48e9b6e75a70
ashutoshiru <ashutoshiruu@gmail.com>
2026-02-07 01:26:43 +0530

    Day-1 of my React

---
+++ 01-Day/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => { 
+  return ( 
+    <> {/*react fragment */}
+    <div> 
+      <h1>This is my First class of React.js</h1>
+    </div>
+    <div>
+      <h2>This is my 2nd div</h2>
+    </div>
+    </>
+  )
+
+  
+}
+
+export default App
+
+
+
+
+
+// let app = () =>{ 
+//   return <h1>This is my First class of React.js</h1>
+// }
+
+// export default app;

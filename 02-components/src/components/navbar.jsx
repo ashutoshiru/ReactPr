86a317dabb55865e9665ba8bb7b492f7ed30f6ad
ashutoshiru <ashutoshiruu@gmail.com>
2026-02-08 00:04:49 +0530

    added components folder

---
+++ 02-components/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    
+    <div>
+      <div className="nav">
+      navbar
+     </div>
+    </div>
+    
+  )
+}
+
+export default navbar
